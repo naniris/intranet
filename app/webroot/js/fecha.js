823313130edfbe4eafc6db4aecad3cc712f65e5d
Jose Jesus Leyva <naniz_leyva@outlook.com>
2018-09-11 10:05:59 -0500

    Se quitaron los comentarios del js

--- app/webroot/js/fecha.js
+++ app/webroot/js/fecha.js
@@ -1,11 +1,11 @@
 window.onload = function(){
-    var fecha = new Date(); //Fecha actual
-    var mes = fecha.getMonth()+1; //obteniendo mes
-    var dia = fecha.getDate(); //obteniendo dia
-    var ano = fecha.getFullYear(); //obteniendo año
+    var fecha = new Date();
+    var mes = fecha.getMonth()+1; 
+    var dia = fecha.getDate(); 
+    var ano = fecha.getFullYear();
     if(dia<10)
-      dia='0'+dia; //agrega cero si el menor de 10
+      dia='0'+dia; 
     if(mes<10)
-      mes='0'+mes //agrega cero si el menor de 10
+      mes='0'+mes 
     document.getElementById('fechaActual').value=ano+"-"+mes+"-"+dia;
   }
